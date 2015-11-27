1586861dac37e75b269739b015643074d99d0b72
xue <xue@xue>
2015-11-27 17:57:56 +0800

    Install as System APPs

--- MyService/Android.mk
+++ MyService/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng
 LOCAL_PACKAGE_NAME := MyService
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PACKAGE)
