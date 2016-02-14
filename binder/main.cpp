01ca6c32451d9e1192d63d5e115808df95f815fe
xxue <xue.xue@polycom.com>
2016-02-14 16:01:07 +0800

    Update binder code

--- binder/main.cpp
+++ binder/main.cpp
@@ -10,16 +10,16 @@ using namespace android;
 int main()
 {
 	static bool _first = true;
-	int g_osd_size = (1920 * 1080 * 3 / 2 + 16 * 4) * 3;
+	int g_osd_size = (1920 * 1080 * 3 / 2 + 16 * 4);
 
 	sp<ProcessState> proc(ProcessState::self());
 	sp<MemoryHeapBase> g_MemHeap = new MemoryHeapBase(g_osd_size, 0,"Oculus.OSDMemory");
+	memset(g_MemHeap->getBase(),0,g_MemHeap->getSize());
 	sp<IServiceManager> sm = defaultServiceManager();
 	int r = defaultServiceManager()->addService(String16("Oculus.OSDMemory"), g_MemHeap);
-	printf("r = %d\n",r);
 
 	proc->startThreadPool();
-	printf("1\n");
+	printf("ok\n");
 
 	while(1) sleep(1);
 	return 0;
--- binder/mainclient.cpp
+++ binder/mainclient.cpp
@@ -12,44 +12,86 @@
 
 using namespace android;
 
+void captureIMG(char *buffer, char * filename)
+{
+	int * enableOSD = (int *) buffer;
+	int * enableCapture = (int *) buffer + 1;
+	int * osdleft = (int *) buffer + 12;
+	int * osdtop = (int *) buffer + 13;
+	int * osdwidth = (int *) buffer + 14;
+	int * osdheight = (int *) buffer + 15;
+	char * imageBase = buffer + 16 * 4;
+
+	//start capture
+	*enableCapture = 1;
+
+	//sleep
+	while (*enableCapture == 1)
+	{
+		printf(".\n");
+		sleep(1);
+	}
+
+	//write file
+	int filedes = open(filename, O_CREAT | O_WRONLY | O_SYNC | O_TRUNC, 0777);
+	if (filedes > 0)
+	{
+		int ret = write(filedes, imageBase, 1920 * 1080 * 3 / 2);
+		close(filedes);
+	}
+}
+
+void OSD(char * buffer, char * filename)
+{
+	int * enableOSD = (int *) buffer;
+	int * enableCapture = (int *) buffer + 1;
+	int * osdleft = (int *) buffer + 12;
+	int * osdtop = (int *) buffer + 13;
+	int * osdwidth = (int *) buffer + 14;
+	int * osdheight = (int *) buffer + 15;
+	char * imageBase = buffer + 16 * 4;
+
+	*osdleft = 640;
+	*osdtop  = 360;
+	*osdwidth = 300;
+	*osdheight = 300;
+
+	//write file
+	int filedes = open(filename, O_RDONLY, 0777);
+	if (filedes > 0)
+	{
+		read(filedes, imageBase, (*osdwidth) * (*osdheight) * 3 / 2);
+		close(filedes);
+	}
+	for (int i = 0; i < (*osdwidth) * (*osdheight) / 4; i++)
+	{
+		char * p = imageBase + (*osdwidth) * (*osdheight) + 2 * i;
+		char temp = *p;
+		*p = *(p + 1);
+		*(p + 1) = temp;
+	}
+
+	*enableOSD = 1;
+
+	while(1) sleep(1);
+}
 
 int main(int argc, char * argv[])
 {
+	//open OSDMemory
 	sp<IServiceManager> sm = defaultServiceManager();
 	sp<IBinder> binder = sm->getService(String16("Oculus.OSDMemory"));
 	printf("Binder:%x\n",binder.get());
 	sp<IMemoryHeap> memHeap = IMemoryHeap::asInterface(binder);
 	char * p = (char *)memHeap->getBase();
-
 	printf("length:%d piont:%x\n",memHeap->getSize(),p);
 
-//	while(1)
-//	{
-//	printf("%d\n",*(int *)p);
-//	(*(int *)p)++;
-//	printf("%d\n",*(int *)p);
-//	sleep(1);
-//	}
-
-//capture
-	if (argc >= 2)
+	if(argc >= 2)
 	{
-		//start capture
-		*((int *) p + 1) = 1;
-		while (*((int *) p + 1) == 1)
-		{
-			printf("%d\n",*((int *) p + 1));
-			sleep(1);
-		}
-
-		int filedes = open(argv[1], O_CREAT | O_WRONLY | O_SYNC | O_TRUNC, 0777);
-		if (filedes > 0)
-		{
-			int ret = write(filedes, (void*) p + 4*16, 1920*1080*3/2);
-			close(filedes);
-		}
+		//captureIMG(p,argv[1]);
+		OSD(p,argv[1]);
 	}
-	printf("wait for exit....\n");
-	while(1) sleep(1);
+
+
 	return 0;
 }
